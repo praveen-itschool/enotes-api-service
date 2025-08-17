daa5006aa29b04de29cb66fdaa68ec16f5cb9c35
praveen-itschool <166507289+praveen-itschool@users.noreply.github.com>
2025-08-17 16:19:36 +0530

    initial commit

---
+++ src/main/java/com/becoder/entity/EnotesProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.becoder.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesProjectsApplication.class, args);
+	}
+
+}
